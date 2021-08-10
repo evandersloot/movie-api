335d5b0c1a608f21905203900690271affe0ddfe
evandersloot <edvandersloot@gmail.com>
2021-08-10 16:10:13 -0500

    corrected typo on index.js

--- index.js
+++ index.js
@@ -109,10 +109,10 @@ app.get('/directors/:Name', passport.authenticate('jwt', { session: false }), (r
 //allow new user to register
 app.post('/users',
   [
-    check('username', 'Username is required').isLength({min: 5}),
-    check('username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-    check('password', 'Password is required.').not().isEmpty(),
-    check('email', 'Email does not appear to be valid.').isEmail()
+    check('username', 'username is required').isLength({min: 5}),
+    check('username', 'username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('password', 'password is required.').not().isEmpty(),
+    check('email', 'email does not appear to be valid.').isEmail()
   ], (req, res) => {
 
     let errors = validationResult(req);
